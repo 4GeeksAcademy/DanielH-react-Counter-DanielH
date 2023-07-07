68a4a46253a68bb0e745a2485bb3f8f194257403
Daniel Hurtado Quero <131759030+OconnelDan@users.noreply.github.com>
2023-07-07 19:15:58 +0000

    proyecto simple finlaizado

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,44 @@
+
+import React from 'react';
+
+const SecondsCounter = (props) => {
+    let unit = props.counter;
+    let ten = 0;
+    let hundred = 0;
+    let thousand = 0;
+    let tenThousand = 0;
+    let hunThousand = 0;
+    if(props.counter > 9){
+        ten =   Math.trunc(props.counter / 10);
+        unit = props.counter % 10;
+        if (ten > 9){
+            hundred =  Math.trunc(ten / 10);
+            ten = ten % 10;
+            if(hundred > 9){
+                thousand = Math.trunc(hundred / 10);
+                hundred = hundred %  10;
+                if(thousand > 9){
+                    tenThousand = Math.trunc(thousand / 10);
+                    thousand = thousand % 10;
+                    if(tenThousand > 9){
+                        hunThousand = Math.trunc(tenThousand / 10);
+                        tenThousand = tenThousand % 10;
+                    }
+                }
+            }
+        }
+    }
+    return (
+        <div className="app d-flex justify-content-center">
+            <div className="icon box"><i className="far fa-clock"></i></div>
+            <div className="counter box">{hunThousand}</div>
+            <div className="counter box">{tenThousand}</div>
+            <div className="counter box">{thousand}</div>
+            <div className="counter box">{hundred}</div>
+            <div className="counter box">{ten}</div>
+            <div className="counter box">{unit}</div>
+        </div>
+    );
+};
+
+export default SecondsCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,19 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import SecondsCounter from "./component/SecondsCounter.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+
+setInterval(() => {
+    // render your react application
+    console.log(counter)
+    ReactDOM.render(<SecondsCounter counter={counter}/>, document.querySelector("#app"));  
+    counter++;
+}, 1000);
